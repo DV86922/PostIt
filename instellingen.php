69eca0feae119bc3376635a2d0fe617c4094eec0
sabrina86924 <74918218+sabrina86924@users.noreply.github.com>
2022-06-01 11:47:15 +0200

    wat ik nu heb gedaan

---
+++ instellingen.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <title>Inloggen</title>
+</head>
+<body>
+    <aside class="instellingen">
+        <header>
+            <h1><i class="fas fa-cog"></i></h1>
+        </header>
+        <div class="tekst">
+            <p><strong>Gebruikersnaam: </strong> SaWor</p>
+            <p><strong>Wachtwoord:</strong>  **********</p>
+            <p><strong>E-mail: </strong> 86924@glr.nl</p>
+            <p id="pasaan"><i class="fas fa-sliders-h"></i></p>
+        </div>
+    </aside>
+
+</body>
