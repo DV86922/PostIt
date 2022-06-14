8f65bc78237a605d07a21fbcf1dd791aa2cbb1f9
sabrina86924 <74918218+sabrina86924@users.noreply.github.com>
2022-06-14 13:55:14 +0200

    inschrijf de goede

--- pages/inschrijf.php
+++ pages/inschrijf.php
@@ -2,82 +2,99 @@
 <html lang="nl">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../media/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Inschrijven</title>
 </head>
 <body>
 
-<div class="main">
-    <?php
-    // De verbinding toevoegen //
-    require 'config.php';
+<?php
+// De verbinding toevoegen //
+require 'config.php';
 
-    $bericht = "";
+$bericht = "";
 
-    if (isset($_POST['inschrijving'])) {
-        // De gebruikersnaam, wachtwoord en email uitlezen //
-        $gebruikersnaam = $_POST['Gebruikersnaam'];
-        $wachtwoord = $_POST['Wachtwoord'];
-        $email = $_POST['Email'];
+if (isset($_POST['inschrijving'])) {
+    // De gebruikersnaam, wachtwoord en email uitlezen //
+    $gebruikersnaam = $_POST['Gebruikersnaam'];
+    $wachtwoord = $_POST['Wachtwoord'];
+    $email = $_POST['Email'];
 
-        // Controleren of de variabelen niet leeg zijn //
-        if ($gebruikersnaam == "" || $wachtwoord == "" || $email == "") {
-            $bericht = "Niet alle velden zijn ingevuld.";
+    // Controleren of de variabelen niet leeg zijn //
+    if ($gebruikersnaam == "" || $wachtwoord == "" || $email == "") {
+        $bericht = "Niet alle velden zijn ingevuld.";
 
-        } // Anders de query invoeren om die variabelen in te vullen in de database //
-        else {
-            // Controleren of de gebruikersnaam al bestaat //
-            $sql = "SELECT * FROM Accounts WHERE Gebruikersnaam='" . $gebruikersnaam . "'";
+    } // Anders de query invoeren om die variabelen in te vullen in de database //
+    else {
+        // Controleren of de gebruikersnaam al bestaat //
+        $sql = "SELECT * FROM Accounts WHERE Gebruikersnaam='" . $gebruikersnaam . "'";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            $bericht = "Dit gebruikersnaam bestaat al!";
+        } else {
+            // Controleren of de email al bestaat //
+            $sql = "SELECT * FROM Accounts WHERE Email='" . $email . "'";
             $result = mysqli_query($mysqli, $sql);
 
             if (mysqli_num_rows($result) > 0) {
-                $bericht = "Dit gebruikersnaam bestaat al!";
+                $bericht = "Dit email is al in gebruik!";
             } else {
-                // Controleren of de email al bestaat //
-                $sql = "SELECT * FROM Accounts WHERE Email='" . $email . "'";
-                $result = mysqli_query($mysqli, $sql);
-
-                if (mysqli_num_rows($result) > 0) {
-                    $bericht = "Dit email bestaat al!";
-                } else {
-                    // het wordt toegevoegd aan de tabel //
-                    $query = "INSERT INTO Accounts";
-                    $query .= " (Gebruikersnaam, Wachtwoord, Email)";
-                    $query .= " VALUES ('{$gebruikersnaam}', md5('{$wachtwoord}'), '{$email}')";
-                }
+                // het wordt toegevoegd aan de tabel //
+                $query = "INSERT INTO Accounts";
+                $query .= " (Gebruikersnaam, Wachtwoord, Email)";
+                $query .= " VALUES ('{$gebruikersnaam}', md5('{$wachtwoord}'), '{$email}')";
             }
         }
-
-        $result = mysqli_query($mysqli, $query);
-
-        if ($result) {
-            echo "Registratie gelukt!";
-            header("Location:login.php");
-        }
-
     }
 
-    ?>
+    $result = mysqli_query($mysqli, $query);
+
+    if ($result) {
+        echo "Registratie gelukt!";
+        header("Location:login.php");
+    }
+
+}
+
+?>
+
+<div class="main">
     <div class="container">
-        <form id="inschrijf" name="inschrijven" method="post" action="">
-            <div class="bericht"><?php if ($bericht != "") {
+        <div class="logo">
+            <img src="logo.png" alt="logo">
+        </div>
+        <div class="text-center mt-4 name">
+            Post it!
+        </div>
+        <form action="" id="inschrijf" method="post">
+            <div class="text-center mt-4 bericht"><?php if ($bericht != "") {
                     echo $bericht;
                 } ?></div>
-            <h2>Inschrijven</h2><br>
-            <label>Gebruikersnaam:</label>
-            <input class="input" type="text" name="Gebruikersnaam" placeholder="JaneDoe" required><br>
-            <label>Wachtwoord:</label>
-            <input class="input" type="password" name="Wachtwoord" placeholder="••••••••" required><br>
-            <label>E-mail:</label>
-            <input class="input" type="email" name="Email" placeholder="12345@glr.nl" required><br>
-
-            <br>
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input class="input" type="text" id="userName" name="Gebruikersnaam" placeholder="Gebruikersnaam"
+                       required>
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input class="input" type="password" id="pwd" name="Wachtwoord" placeholder="Wachtwoord" required>
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-envelope"></span>
+                <input class="input" type="email" id="mail" name="Email" placeholder="E-mail" required>
+            </div>
             <div class="center">
-                <input class="btn" type="submit" name="inschrijving" value="Verzend">
-                <input class="btn" type="reset">
+                <input class="btn mt-3" type="submit" name="inschrijving" value="Verzend">
+                <input class="btn mt-3" type="reset">
             </div>
         </form>
     </div>
 </div>
+
 
 </body>
