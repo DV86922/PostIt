2bf8f8a0fcbc4dbac5d281e0a5f09a0e16886205
sabrina86924 <74918218+sabrina86924@users.noreply.github.com>
2022-06-17 13:56:38 +0200

    laatste versie

--- pages/dagen/dag.php
+++ pages/dagen/dag.php
@@ -11,20 +11,20 @@ $dagID = $_GET['Dag'];
 // haal de naam uit de URL
 $Naam = $_GET['Naam'];
 
-$query = "SELECT Dagen FROM `Dagen` WHERE DagID = {$dagID}";
+$queryDagen = "SELECT Dagen FROM `Dagen` WHERE DagID = {$dagID}";
 
-$result = mysqli_query($mysqli, $query);
+$resultDagen = mysqli_query($mysqli, $queryDagen);
 
-if (mysqli_num_rows($result) > 0) {
-while ($item = mysqli_fetch_assoc($result)) {
+if (mysqli_num_rows($resultDagen) > 0) {
+while ($itemDagen = mysqli_fetch_assoc($resultDagen)) {
 
 
-$query = "SELECT * FROM `Accounts` WHERE Gebruikersnaam = '{$Naam}'";
+$queryAccounts = "SELECT * FROM `Accounts` WHERE Gebruikersnaam = '{$Naam}'";
 // De query uitgevoerd en het resultaat opgevangen //
-$result = mysqli_query($mysqli, $query);
+$resultAccounts = mysqli_query($mysqli, $queryAccounts);
 
-if (mysqli_num_rows($result) > 0) {
-while ($item = mysqli_fetch_assoc($result)) {
+if (mysqli_num_rows($resultAccounts) > 0) {
+while ($item = mysqli_fetch_assoc($resultAccounts)) {
 $inGebruiker = new Gebruiker($item['GebruikerID'], $item['Gebruikersnaam'], $item['Wachtwoord'], $item['Email']);
 //GebruikerID veranderen in een getal //
 $gebruikerID = intval($inGebruiker->gebruikerID);
@@ -41,46 +41,45 @@ $gebruikerID = intval($inGebruiker->gebruikerID);
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
             crossorigin="anonymous"></script>
     <script src="ajax.js" defer></script>
-    <title><?= $item['Dagen'] ?></title>
+    <title><?= $itemDagen['Dagen'] ?></title>
 </head>
 <body>
 <?php
 require_once "header_responsive.php";
 ?>
-<h1 class="dag"><?= strtoupper($item['Dagen']) ?></h1> <?php }
-} ?>
+<h1 class="dag"><?= strtoupper($itemDagen['Dagen']) ?></h1> <?php }
+}
+?>
 <div class="dagen">
     <form id="zoek">
-        <!--    Hier moet ik met de class de ingelogde gebruiker hebben. dan kan ik de id hebben. die controleer ik met tabel in taken op ajax.php-->
-        <input id="naam" type="text" name="id" value="<?= $gebruikerID ?>">
+        <input id="naamID" type="text" name="naamID" value="<?= $gebruikerID ?>">
+        <input id="dagID" type="text" name="dagID" value="<?= $dagID ?>">
+        <input id="naam" type="text" name="naam" value="<?= $Naam ?>">
         <label for="search">Search</label>
         <input id="search" type="search" name="zoek" pattern=".*\S.*" required>
         <span class="caret"></span>
         <div id="resultaat"></div>
     </form>
 
-    <button class="knop lichtRood" id="lichtGrijsPostIt"></button>
-    <button class="knop lichtGeel" id="lichtGeelPostIt"></button>
-    <button class="knop lichtBlauw" id="lichtBlauwPostIt"></button>
-    <button class="knop lichtGroen" id="lichtGroenPostIt"></button>
-    <ul>
+    <ul id="result" class="alles">
         <?php
         // Query controleren of gebruikerID en dagID goed zijn //
-        $query2 = "SELECT * FROM `Taken` WHERE GebruikerID = {$gebruikerID} AND DagID = {$dagID}";
+        $query = "SELECT * FROM `Taken` WHERE GebruikerID = {$gebruikerID} AND DagID = {$dagID}";
 
-        $result2 = mysqli_query($mysqli, $query2);
-        if (mysqli_num_rows($result2) > 0) {
-            while ($item = mysqli_fetch_assoc($result2)) {
+        $result = mysqli_query($mysqli, $query);
+        if (mysqli_num_rows($result) > 0) {
+            while ($item = mysqli_fetch_assoc($result)) {
                 // Tijd in de juiste format //
                 $TijdBegin = new DateTime($item['BeginTijd']);
                 $TijdEind = new DateTime($item['EindTijd']);
                 ?>
                 <li>
-                    <div class="postIt">
+                    <div style="background-color: <?= $item['Kleur'] ?>" class="postIt">
                         <p class="tijd"><?= $TijdBegin->format('H:i'); ?> - <?= $TijdEind->format('H:i'); ?></p>
-                        <h2><?= $item['TaakTitel'] ?></h2>
+                        <h2 id="zoekPostIt"><?= $item['TaakTitel'] ?></h2>
                         <p id="omschrijving"><?= $item['TaakOmschrijving'] ?></p>
-                        <p class="pasaan"><a href="../pasaan.php?Naam=<?= $Naam ?>&id=<?= $item['TaakID'] ?>&Dag=<?= $dagID ?>"><i
+                        <p class="pasaan"><a
+                                    href="../pasaan.php?Naam=<?= $Naam ?>&id=<?= $item['TaakID'] ?>&Dag=<?= $dagID ?>"><i
                                         class="fas fa-edit"></i></a></p>
                     </div>
                 </li>
