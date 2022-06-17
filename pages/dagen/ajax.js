2a133d6620a2861b7f7ec0d5cbeb53572013d0b2
sabrina86924 <74918218+sabrina86924@users.noreply.github.com>
2022-06-17 13:55:55 +0200

    laatste versie

--- pages/dagen/ajax.js
+++ pages/dagen/ajax.js
@@ -1,25 +1,45 @@
-$("#search").keyup(function(){
+$("#search").keyup(function () {
     //Lees het veld uit
     let zoek = $('#search').val();
+    let naamID = $('#naamID').val();
+    let dagID = $('#dagID').val();
     let naam = $('#naam').val();
     //Controleer of het veld is in gevuld
-    if (zoek === "")
-    {
-        $("#resultaat").html("");
-    }
-    //Als het veld is ingevuld, verstuur de data naar de verwerkpagina
-    else
-    {
+    if (zoek !== "") {
+        // Alleen degene die de value heeft van zoek tonen
         $.ajax({
-            type:   "POST",
-            url:    "ajax.php",
-            data:   {"zoek": zoek,
-                    "naam": naam},
+            type: "POST",
+            url: "ajax.php",
+            data: {
+                "zoek": zoek,
+                "naamID": naamID,
+                "dagID": dagID,
+                "naam": naam
+            },
             success: function (tekst) {
-                $("#resultaat").html(tekst);
+                $("#result").html(tekst);
             },
             error: function (request, error) {
-                console.log ("FOUT:" + error);
+                console.log("FOUT:" + error);
+            }
+        });
+    }
+    //Als er niks is ingevuld toon de hele lijst
+    else {
+        $.ajax({
+            type: "POST",
+            url: "allePostIt.php",
+            data: {
+                "zoek": zoek,
+                "naamID": naamID,
+                "dagID": dagID,
+                "naam": naam
+            },
+            success: function (tekst) {
+                $("#result").html(tekst);
+            },
+            error: function (request, error) {
+                console.log("FOUT:" + error);
             }
         });
     }
@@ -28,25 +48,17 @@ $("#search").keyup(function(){
 });
 
 
-// Kleur van dew post it's aanpassen//
-
-let geselecteerdePostIt = null;
-$("body").on("click", ".postIt", function (e) {
-    geselecteerdePostIt= $(this);
-    $(".postIt").removeClass("geselecteerd");
-    $(this).addClass("geselecteerd");
-
-});
-
-$("#lichtGrijsPostIt").click(function () {
-    $(geselecteerdePostIt).css("background-color", "lightcoral");
+$("#lichtRoodPostIt").click(function () {
+    $('.postIt').css("background-color", "lightcoral");
 })
-$("#lichtGeelMPostIt").click(function () {
-    $(geselecteerdePostIt).css("background-color", "lightyellow");
+$("#lichtGeelPostIt").click(function () {
+    $('.postIt').css("background-color", "lightyellow");
 })
 $("#lichtBlauwPostIt").click(function () {
-    $(geselecteerdePostIt).css("background-color", "lightblue");
+    $('.postIt').css("background-color", "lightblue");
 })
 $("#lichtGroenPostIt").click(function () {
-    $(geselecteerdePostIt).css("background-color", "lightgreen");
+    $('.postIt').css("background-color", "lightgreen");
 })
+
+
