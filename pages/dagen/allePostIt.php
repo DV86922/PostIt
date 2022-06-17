752d7425cb23f5ad6810b4e5e06e8429c83d8956
sabrina86924 <74918218+sabrina86924@users.noreply.github.com>
2022-06-17 13:57:10 +0200

    laatste versie

---
+++ pages/dagen/allePostIt.php
@@ -0,0 +1,33 @@
+<?php
+require '../config.php';
+// zoek uitlezen
+$zoek = $_POST['zoek'];
+$naamID = $_POST['naamID'];
+$dagID = $_POST['dagID'];
+$Naam = $_POST['naam'];
+
+// Query controleren of gebruikerID en dagID goed zijn //
+$query = "SELECT * FROM Taken WHERE GebruikerID = {$naamID} AND DagID = {$dagID}";
+$result = mysqli_query($mysqli, $query);
+if (mysqli_num_rows($result) > 0) {
+    while ($item = mysqli_fetch_assoc($result)) {
+        // Tijd in de juiste format //
+        $TijdBegin = new DateTime($item['BeginTijd']);
+        $TijdEind = new DateTime($item['EindTijd']);
+        ?>
+        <li>
+            <div style="background-color: <?= $item['Kleur'] ?>" class="postIt">
+                <p class="tijd"><?= $TijdBegin->format('H:i'); ?> - <?= $TijdEind->format('H:i'); ?></p>
+                <h2 id="zoekPostIt"><?= $item['TaakTitel'] ?></h2>
+                <p id="omschrijving"><?= $item['TaakOmschrijving'] ?></p>
+                <p class="pasaan"><a
+                            href="../pasaan.php?Naam=<?= $Naam ?>&id=<?= $item['TaakID'] ?>&Dag=<?= $dagID ?>"><i
+                                class="fas fa-edit"></i></a></p>
+            </div>
+        </li>
+        <?php
+    }
+}
+?>
+
+
